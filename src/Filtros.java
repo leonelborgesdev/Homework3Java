13db5b943fc69e593b3356b1207ef64f087afbfe
leonelborgesdev <leonel03t@gmail.com>
2024-01-07 21:31:12 -0400

    clase filtros

---
+++ src/Filtros.java
@@ -0,0 +1,18 @@
+public class Filtros {
+    public static Filtrable vendible(){
+        return producto -> producto.getPrecio()>0;
+    }
+
+    public static Filtrable descartable(){
+        return producto -> producto.getPrecio()>0;
+    }
+
+    public static Filtrable imperecedero() throws Exception{
+        try{
+            return producto -> producto.getCodigo().equals("10");
+        }catch (Exception e){
+            return (Filtrable) new Exception("No es imperecedero");
+        }
+
+    }
+}
